3b1d98b2180a313ecefbc9c5e9f00b4ebaa9fef2
manymany.macarons <manymany.macarons@gmail.com>
2018-12-29 11:10:54 +0900

    getView 메쏘드 내부 작업

--- app/src/main/java/kr/tjit/a20181229_01_spinnertest/adapters/CompanyAdapter.java
+++ app/src/main/java/kr/tjit/a20181229_01_spinnertest/adapters/CompanyAdapter.java
@@ -5,6 +5,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -31,11 +33,17 @@ public class CompanyAdapter extends ArrayAdapter<Restaurant> {
         View row = convertView;
 
         if (row == null) {
-
 //            row야 너 화면 좀 그려
             row = inf.inflate(R.layout.company_list_item, null);
-
         }
+
+        ImageView logoImg = row.findViewById(R.id.logoImg);
+        TextView nameTxt = row.findViewById(R.id.nameTxt);
+
+//        근거가 될 아이템을 받아오자
+        Restaurant data = mList.get(position);
+
+        nameTxt.setText(data.getName());
 
         return row;
     }
